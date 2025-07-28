cd784949e6bc120e44a24baa336e111f9cc8f9a4
Wonho Kim <wonho.kim99@gmail.com>
2025-07-28 18:28:05 +0900

    [FEAT] SpringBoot 환경 설정 파일 세팅

---
+++ src/main/java/thonlivethondie/artcorner/ArtcornerApplication.java
@@ -0,0 +1,13 @@
+package thonlivethondie.artcorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtcornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtcornerApplication.class, args);
+	}
+
+}
